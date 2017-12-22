cc39d573a28a870b45589d502ba45687b6d93c94
hayaken8112 <hayashi8112@gmail.com>
2017-12-23 08:57:51 +0900

    センサーデータを双方向に遅れるようにした

--- test/esp_client/esp_client.ino
+++ test/esp_client/esp_client.ino
@@ -1,8 +1,10 @@
-
-//https://github.com/Links2004/arduinoWebSockets/issues/33
+#include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <WebSocketsClient.h> //https://github.com/Links2004/arduinoWebSockets
-//myip 27
+#include <Wire.h>                             //I2Cライブラリ
+#include <SparkFunLSM9DS1.h>                  //https://github.com/sparkfun/SparkFun_LSM9DS1_Arduino_Library
+#include <Adafruit_NeoPixel.h>
+
 
 //IP
 const char* Server_ip("192.168.11.31");
@@ -14,6 +16,45 @@ const char* password = "bnhcsau8trtk3";
 
 WebSocketsClient webSocket;
 
+#define MAX_VAL 20  // 0 to 255 for brightness
+#define DELAY_TIME 50 
+#define DELAY_TIME2 20
+#define ADAddr 0x48
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(30, 0, NEO_GRB + NEO_KHZ800);
+
+//-------------------------------------------------------------------------
+float gxVal = 0;                                //ジャイロｘ軸用データーレジスタ
+float gyVal = 0;                                //ジャイロｙ軸用データーレジスタ
+float gzVal = 0;                                //ジャイロｚ軸用データーレジスタ
+float axVal = 0;                                //Axis ｘ用データーレジスタ
+float ayVal = 0;                                //Axis ｙ用データーレジスタ
+float azVal = 0;                                //Axis ｚ用データーレジスタ
+float mxVal = 0;                                //Mag x 用データーレジスタ
+float myVal = 0;                                //Mag ｙ 用データーレジスタ
+float mzVal = 0;                                //Mag x 用データーレジスタ
+float hedVal = 0;                               //Hedding 用データーレジスタ
+
+//------------------------------------------------------------------------
+LSM9DS1 imu;
+
+#define NUM_OF_DATA 3
+
+uint8_t* packet;
+float send_data[NUM_OF_DATA];
+float heading = 0;
+float roll = 0;
+float pitch = 0;
+
+float* rec_data;
+
+size_t packet_size = 12;
+
+#define LSM9DS1_M  0x1E // SPIアドレス設定 0x1C if SDO_M is LOW
+#define LSM9DS1_AG  0x6B // SPIアドレス設定 if SDO_AG is LOW
+
+#define PRINT_CALCULATED
+#define PRINT_SPEED 250 // 250 ms between prints
+#define DECLINATION -8.58 // Declination (degrees) in Boulder, CO.
 
 void webSocketEvent(WStype_t type, uint8_t * payload, size_t lenght) {
 
@@ -35,13 +76,17 @@ void webSocketEvent(WStype_t type, uint8_t * payload, size_t lenght) {
             //webSocket.sendTXT(payload, lenght);
             break;
 
-        case WStype_BIN:
+        case WStype_BIN: {
             Serial.printf("[WSc] get binary lenght: %u\n", lenght);
+            
             hexdump(payload, lenght);
 
+            rec_data = (float *) payload;
+
             // echo data back to Server
-            webSocket.sendBIN(payload, lenght);
+            //webSocket.sendBIN(payload, lenght);
             break;
+        }
 
         case WStype_ERROR:
             Serial.printf("We received an error \n");
@@ -73,10 +118,49 @@ void setup() {
     webSocket.onEvent(webSocketEvent);
     Serial.println("Client started\n");
 
+    imu.settings.device.commInterface = IMU_MODE_I2C;
+    imu.settings.device.mAddress = LSM9DS1_M;
+    imu.settings.device.agAddress = LSM9DS1_AG;
+
+    if (!imu.begin()) {
+        // センサーエラー
+        Serial.println("Failed to communicate with LSM9DS1.");
+        Serial.println("Double-check wiring.");
+        Serial.println("Default settings in this sketch will " \
+                    "work for an out of the box LSM9DS1 " \
+                    "Breakout, but may need to be modified " \
+                    "if the board jumpers are.");
+        while (1)
+        ;
+    }
+    Serial.printf("Sensor set");
+
+}
+
+void printComData(float* data){
+  Serial.print("heading, roll, pitch\n");
+    for (int i = 0; i < NUM_OF_DATA; i++) {
+    Serial.print(data[i]);
+    Serial.print(" ");
+  }
+  Serial.println();
 }
 
 
 void loop() {
+    static unsigned long last = 0;
     webSocket.loop();
-    webSocket.sendTXT("hello"); //send
+    calcSensorData();
+    send_data[0] = heading;
+    send_data[1] = roll;
+    send_data[2] = pitch;
+    if(abs(millis() - last) > 10) {
+      packet = (uint8_t *) send_data;
+      webSocket.sendBIN(packet,packet_size);
+      last = millis();
+    }
+      Serial.print("my data\n");
+      printComData(send_data);
+      Serial.print("received data\n");
+      if(rec_data != NULL) printComData(rec_data);
 }
--- test/esp_server/esp_server.ino
+++ test/esp_server/esp_server.ino
@@ -1,7 +1,9 @@
-//https://github.com/Links2004/arduinoWebSockets/issues/33
+#include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <WebSocketsServer.h>
-//myip 28
+#include <Wire.h>                             //I2Cライブラリ
+#include <SparkFunLSM9DS1.h>                  //https://github.com/sparkfun/SparkFun_LSM9DS1_Arduino_Library
+#include <Adafruit_NeoPixel.h>
 
 //IP
 IPAddress ip(192, 168, 11, 31);
@@ -16,6 +18,47 @@ const char* password = "bnhcsau8trtk3";
 
 
 WebSocketsServer webSocket = WebSocketsServer(port);
+
+#define MAX_VAL 20  // 0 to 255 for brightness
+#define DELAY_TIME 50 
+#define DELAY_TIME2 20
+#define ADAddr 0x48
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(30, 0, NEO_GRB + NEO_KHZ800);
+
+//-------------------------------------------------------------------------
+float gxVal = 0;                                //ジャイロｘ軸用データーレジスタ
+float gyVal = 0;                                //ジャイロｙ軸用データーレジスタ
+float gzVal = 0;                                //ジャイロｚ軸用データーレジスタ
+float axVal = 0;                                //Axis ｘ用データーレジスタ
+float ayVal = 0;                                //Axis ｙ用データーレジスタ
+float azVal = 0;                                //Axis ｚ用データーレジスタ
+float mxVal = 0;                                //Mag x 用データーレジスタ
+float myVal = 0;                                //Mag ｙ 用データーレジスタ
+float mzVal = 0;                                //Mag x 用データーレジスタ
+float hedVal = 0;                               //Hedding 用データーレジスタ
+
+//------------------------------------------------------------------------
+LSM9DS1 imu;
+
+#define NUM_OF_DATA 3
+
+uint8_t* packet;
+float send_data[NUM_OF_DATA];
+float heading = 0;
+float roll = 0;
+float pitch = 0;
+
+float* rec_data;
+
+size_t packet_size = 12;
+
+#define LSM9DS1_M  0x1E // SPIアドレス設定 0x1C if SDO_M is LOW
+#define LSM9DS1_AG  0x6B // SPIアドレス設定 if SDO_AG is LOW
+
+#define PRINT_CALCULATED
+#define PRINT_SPEED 250 // 250 ms between prints
+#define DECLINATION -8.58 // Declination (degrees) in Boulder, CO.
+
 
 
 void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
@@ -41,13 +84,20 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length
             //webSocket.broadcastTXT(payload, length);
             break;
 
-        case WStype_BIN:
+        case WStype_BIN: {
+ 
             Serial.printf("[%u] get binary length: %u\n", num, length);
-            hexdump(payload, length);
-            // echo data back to browser
-            webSocket.sendBIN(num, payload, length);
-            break;
 
+            hexdump(payload, length);
+
+            rec_data = (float *) payload;
+     
+
+            // echo data back to browser
+            //webSocket.sendBIN(num, payload, length);
+            break;
+        }
+            
         case WStype_ERROR:
             break;
 
@@ -57,6 +107,7 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length
 
 }
 
+//----------------------　setup　---------------------------
 void setup() {
     Serial.begin(115200);
     delay(100);
@@ -78,8 +129,52 @@ void setup() {
     webSocket.begin();
     webSocket.onEvent(webSocketEvent);
     Serial.printf("Server Started\n");
+
+
+    imu.settings.device.commInterface = IMU_MODE_I2C;
+    imu.settings.device.mAddress = LSM9DS1_M;
+    imu.settings.device.agAddress = LSM9DS1_AG;
+
+    if (!imu.begin()) {
+        // センサーエラー
+        Serial.println("Failed to communicate with LSM9DS1.");
+        Serial.println("Double-check wiring.");
+        Serial.println("Default settings in this sketch will " \
+                    "work for an out of the box LSM9DS1 " \
+                    "Breakout, but may need to be modified " \
+                    "if the board jumpers are.");
+        while (1)
+        ;
+    }
+    Serial.printf("Sensor set");
 }
 
+void printComData(float* data){
+  Serial.print("heading, roll, pitch\n");
+  for (int i = 0; i < NUM_OF_DATA; i++) {
+    Serial.print(data[i]);
+    Serial.print(" ");
+  }
+  Serial.println();
+}
+
+//-------------------------　メインループ　--------------------------------
 void loop() {
-    webSocket.loop();
+      static unsigned long last = 0;
+      webSocket.loop();
+      calcSensorData();
+      send_data[0] = heading;
+      send_data[1] = roll;
+      send_data[2] = pitch;
+    if(abs(millis() - last) > 10) {
+        packet = (uint8_t *) send_data;
+        webSocket.sendBIN(0, packet,packet_size);
+        last = millis();
+    }
+        Serial.print("my data\n");
+        printComData(send_data);
+        Serial.println();
+        Serial.print("received data\n");
+        if (rec_data != NULL) printComData(rec_data);
+    
 }
